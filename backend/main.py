e0819f77d48e91a6f333a16dc50d6457d16b7d25
Dom Grad <Dominikgrad47@gmail.com>
2024-05-12 13:45:00 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,7 @@ load_dotenv()
 s3_client = boto3.client('s3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name = 'us-east-2',
 )
 app = FastAPI()
 
